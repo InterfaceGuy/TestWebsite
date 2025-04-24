221ff667b2b2f994a5a6efe71f249ea55f70b533
InterfaceGuy <65857060+InterfaceGuy@users.noreply.github.com>
2025-04-24 12:52:51 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://InterfaceGuy.github.io'
+  site: 'https://InterfaceGuy.github.io',
+  base: 'TestWebsite'
 });
